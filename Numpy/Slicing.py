4a9d4966dffb4c6489ba99906e30a733bad07628
Harshit garg2611 <hgarg4145@gmail.com>
2025-08-05 02:18:55 +0530

    numpy

--- Numpy/Slicing.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-var = np.array([4,2,7,1,8,5,3,0])
-print(var[2:7:2])
--- Numpy/split.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-var = np.array([1,2,3,4,5,6])
-
-# arr = np.split(var, 3)
-arr = np.array_split(var, 3)
-# print(arr)
-
-# print(type(arr))
